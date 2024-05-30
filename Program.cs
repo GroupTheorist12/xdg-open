ec960111dcb70b3a271abc755fa96da827d5532d
GroupTheorist12 <55472994+GroupTheorist12@users.noreply.github.com>
2024-05-30 07:37:30 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace xdg_open
+{
+   class Program
+   {
+      public static void OpenBrowser(string url)
+      {
+         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+         {
+            Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+         }
+         else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+         {
+            Process.Start("xdg-open", url);
+         }
+         else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+         {
+            Process.Start("open", url);
+         }
+         else
+         {
+            throw new System.Exception("Unsupported operating system.");
+         }
+      }
+      static void Main(string[] args)
+      {
+         if(args.Length == 1)
+         {
+            OpenBrowser(args[0]);
+         }
+         else
+         {
+            Console.WriteLine("usage is xdg-open url");
+         }
+      }
+   }
+}
